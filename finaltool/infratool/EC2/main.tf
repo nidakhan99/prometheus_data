0b366ea4c8324db391935e50e93f1c46020ed294
nidakhan99 <153353850+nidakhan99@users.noreply.github.com>
2024-03-01 16:45:55 +0530

    Update main.tf

--- finaltool/infratool/EC2/main.tf
+++ finaltool/infratool/EC2/main.tf
@@ -31,42 +31,35 @@ data "aws_ami" "my_ami" {
 #############################[ BESTION HOST ]################################################
 
 resource "aws_instance" "bestion" {
-  count                         = 1
   ami                           = data.aws_ami.my_ami.id
   instance_type                 = var.ec2_type
   key_name                      = var.key
-  subnet_id                     = var.pub_sub[count.index]
+  subnet_id                     = var.pub_sub[0]
   associate_public_ip_address   = true
   security_groups               = [var.sg_id]
 
   provisioner "remote-exec" {
-    inline = [
-  "echo 'Wait until SSH is ready'",
-  "sleep 60",
-]
-  
+    inline = ["echo 'Wait until SSH is ready'"]
+
     connection {
       type        = "ssh"
       user        = var.ssh_user
       private_key = file(var.private_key_path)
-      host        = aws_instance.bestion[0].public_ip
+      host        = self.public_ip
     }
   }
 
   provisioner "local-exec" {
-    command = "ansible-playbook  -i ${aws_instance.bestion[0].public_ip}, --private-key ${var.private_key_path}  ${var.file_name}"
-    
-  }
-  provisioner "local-exec" {
-    command = "ansible-playbook  -i ${aws_instance.bestion[0].public_ip}, --private-key ${var.private_key_path}  ${var.aler_file}"
-    
+    command = "ansible-playbook -i ${self.public_ip}, --private-key ${var.private_key_path} ${var.file_name}"
   }
 
   provisioner "local-exec" {
-    command = "ansible-playbook -i ${aws_instance.bestion[0].public_ip}, --private-key /home/ubuntu/ohio_key.pem ${var.node_file_name}"
+    command = "ansible-playbook -i ${self.public_ip}, --private-key ${var.private_key_path} ${var.aler_file}"
   }
 
-
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ${self.public_ip}, --private-key /home/ubuntu/ohio_key.pem ${var.node_file_name}"
+  }
 
   tags = {
     Name = var.bestion
