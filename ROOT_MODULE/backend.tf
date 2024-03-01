f036ba1ec0037d3bcd21e248d0210e668c452e69
nidakhan99 <153353850+nidakhan99@users.noreply.github.com>
2024-03-01 16:06:10 +0530

    Update backend.tf

--- ROOT_MODULE/backend.tf
+++ ROOT_MODULE/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-tool-buck"
+    bucket = "my-tool-buck2"
     key    = "Terraform_as4/terraform.tfstate"
     region = "us-east-2"
-    dynamodb_table = "tool-lock-db"
+    dynamodb_table = "my_tool_dynamo"
   }
 }
